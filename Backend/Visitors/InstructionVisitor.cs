004b9c726419170106e0ce446c7b0d83a014bad1
Edgardo Zoppi <edgarzpp@gmail.com>
2016-09-23 18:56:00 -0300

    Adding default visitors.

--- Backend/Visitors/InstructionVisitor.cs
+++ Backend/Visitors/InstructionVisitor.cs
@@ -18,37 +18,38 @@ namespace Backend.Visitors
 			}
 		}
 
-		public virtual void Visit(Instruction instruction) { }
-		public virtual void Visit(DefinitionInstruction instruction) { }
-		public virtual void Visit(BinaryInstruction instruction) { }
-		public virtual void Visit(UnaryInstruction instruction) { }
-		public virtual void Visit(LoadInstruction instruction) { }
-		public virtual void Visit(StoreInstruction instruction) { }
-		public virtual void Visit(NopInstruction instruction) { }
-		public virtual void Visit(BreakpointInstruction instruction) { }
-		public virtual void Visit(TryInstruction instruction) { }
-		public virtual void Visit(FaultInstruction instruction) { }
-		public virtual void Visit(FinallyInstruction instruction) { }
-		public virtual void Visit(CatchInstruction instruction) { }
-		public virtual void Visit(ConvertInstruction instruction) { }
-		public virtual void Visit(ReturnInstruction instruction) { }
-		public virtual void Visit(ThrowInstruction instruction) { }
-		public virtual void Visit(BranchInstruction instruction) { }
-		public virtual void Visit(ExceptionalBranchInstruction instruction) { }
-		public virtual void Visit(UnconditionalBranchInstruction instruction) { }
-		public virtual void Visit(ConditionalBranchInstruction instruction) { }
-		public virtual void Visit(SwitchInstruction instruction) { }
-		public virtual void Visit(SizeofInstruction instruction) { }
-		public virtual void Visit(LoadTokenInstruction instruction) { }
-		public virtual void Visit(MethodCallInstruction instruction) { }
-		public virtual void Visit(IndirectMethodCallInstruction instruction) { }
-		public virtual void Visit(CreateObjectInstruction instruction) { }
-		public virtual void Visit(CopyMemoryInstruction instruction) { }
-		public virtual void Visit(LocalAllocationInstruction instruction) { }
-		public virtual void Visit(InitializeMemoryInstruction instruction) { }
-		public virtual void Visit(InitializeObjectInstruction instruction) { }
-		public virtual void Visit(CopyObjectInstruction instruction) { }
-		public virtual void Visit(CreateArrayInstruction instruction) { }
-		public virtual void Visit(PhiInstruction instruction) { }
+		public virtual void Default(Instruction instruction) { }
+		public virtual void Visit(Instruction instruction) { } // Default(instruction); }
+		public virtual void Visit(DefinitionInstruction instruction) { } // { Default(instruction); }
+		public virtual void Visit(BinaryInstruction instruction) { Default(instruction); }
+		public virtual void Visit(UnaryInstruction instruction) { Default(instruction); }
+		public virtual void Visit(LoadInstruction instruction) { Default(instruction); }
+		public virtual void Visit(StoreInstruction instruction) { Default(instruction); }
+		public virtual void Visit(NopInstruction instruction) { Default(instruction); }
+		public virtual void Visit(BreakpointInstruction instruction) { Default(instruction); }
+		public virtual void Visit(TryInstruction instruction) { Default(instruction); }
+		public virtual void Visit(FaultInstruction instruction) { Default(instruction); }
+		public virtual void Visit(FinallyInstruction instruction) { Default(instruction); }
+		public virtual void Visit(CatchInstruction instruction) { Default(instruction); }
+		public virtual void Visit(ConvertInstruction instruction) { Default(instruction); }
+		public virtual void Visit(ReturnInstruction instruction) { Default(instruction); }
+		public virtual void Visit(ThrowInstruction instruction) { Default(instruction); }
+		public virtual void Visit(BranchInstruction instruction) { } // { Default(instruction); }
+		public virtual void Visit(ExceptionalBranchInstruction instruction) { Default(instruction); }
+		public virtual void Visit(UnconditionalBranchInstruction instruction) { Default(instruction); }
+		public virtual void Visit(ConditionalBranchInstruction instruction) { Default(instruction); }
+		public virtual void Visit(SwitchInstruction instruction) { Default(instruction); }
+		public virtual void Visit(SizeofInstruction instruction) { Default(instruction); }
+		public virtual void Visit(LoadTokenInstruction instruction) { Default(instruction); }
+		public virtual void Visit(MethodCallInstruction instruction) { Default(instruction); }
+		public virtual void Visit(IndirectMethodCallInstruction instruction) { Default(instruction); }
+		public virtual void Visit(CreateObjectInstruction instruction) { Default(instruction); }
+		public virtual void Visit(CopyMemoryInstruction instruction) { Default(instruction); }
+		public virtual void Visit(LocalAllocationInstruction instruction) { Default(instruction); }
+		public virtual void Visit(InitializeMemoryInstruction instruction) { Default(instruction); }
+		public virtual void Visit(InitializeObjectInstruction instruction) { Default(instruction); }
+		public virtual void Visit(CopyObjectInstruction instruction) { Default(instruction); }
+		public virtual void Visit(CreateArrayInstruction instruction) { Default(instruction); }
+		public virtual void Visit(PhiInstruction instruction) { Default(instruction); }
 	}
 }
